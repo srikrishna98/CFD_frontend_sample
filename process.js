06d84a92411a0ffa9e28278faad047d44ce49ecf
srikrishna98 <srikrishna0412@gmail.com>
2018-01-16 23:04:24 +0530

    added js

---
+++ process.js
@@ -0,0 +1,5 @@
+var button = document.getElementById("go_button");
+button.addEventListener("click", function() {
+  console.log(document.getElementById("Text").value);
+});
+//tag1.innerHTML="<li><a href=\"#\">"+str+"</a></li>"
